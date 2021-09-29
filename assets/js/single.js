20dbae77af60ae4f652dfe317272ad9202eb975a
Gibby <rgibson@bulldogsolutions.ca>
2021-09-29 17:51:17 -0400

    display issues and warnings

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 
 // ******  GET REPOSITORY ISSUES  ******//
@@ -9,6 +10,11 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -55,6 +61,18 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 
+};
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
